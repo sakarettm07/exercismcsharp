ad299815b6919ae58b1ca10ba490970630920353
mimidealerteam <mimi@dealerteam.com>
2016-07-26 13:15:04 -0400

    word count in progress

---
+++ Word-Count/Phrase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace word_count {
+	class Phrase {
+		internal static Dictionary<string, int> WordCount(string v) {
+			v = v.Trim();
+			string pattern = @"\W+";
+			var rgx = new Regex(pattern);
+			string[] result = rgx.Split(v);
+
+			var count = new Dictionary<string, int>();
+
+			foreach (var item in result) {
+				if (item.Length > 0) {
+					if (count.ContainsKey(item)) {
+						count[item] += 1;
+					}
+					else count.Add(item, 1);
+				}
+			}
+			foreach (var item in count.Keys) {
+				Console.Write(item);
+				Console.WriteLine(count[item]);
+			}
+
+			return count;
+		}
+	}
+}
