f31ab93503b2f2e9161820c7c4cc800a7c8bdd07
mimidealerteam <mimi@dealerteam.com>
2016-07-27 12:42:18 -0400

    Second iteration of Word Count solution
    Correctly handle single and double quotes by trimming individual words
    of leading and trailing single and double quotes.

--- Word-Count/Phrase.cs
+++ Word-Count/Phrase.cs
@@ -5,19 +5,20 @@ using System.Text.RegularExpressions;
 namespace word_count {
 	class Phrase {
 		internal static Dictionary<string, int> WordCount(string v) {
-			v = v.Trim();
-			string pattern = @"\W+";
+			v = v.Trim().ToLower();
+			string pattern = @"[ ,]";
 			var rgx = new Regex(pattern);
 			string[] result = rgx.Split(v);
 
 			var count = new Dictionary<string, int>();
 
 			foreach (var item in result) {
-				if (item.Length > 0) {
-					if (count.ContainsKey(item)) {
-						count[item] += 1;
+				var word = item.Trim(new char[] { ' ', '.', ',', ':', '!', '?', '#', '@', '%', '$', '^', '&', '\'' });
+				if (word.Trim().Length > 0) {
+					if (count.ContainsKey(word)) {
+						count[word] += 1;
 					}
-					else count.Add(item, 1);
+					else count.Add(word, 1);
 				}
 			}
 			foreach (var item in count.Keys) {
